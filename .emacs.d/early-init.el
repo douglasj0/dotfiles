3854ef7de4790110b6872b2ecc4f6e05bcc692c0
Douglas Jackson <djackson@grubhub.com>
2023-10-07 21:57:55 -0500

    modified gc collection numbers, trying tree-sitter again

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -7,33 +7,62 @@
 ; package and UI initialization happens, and before site files are loaded.
 ;;; Code:
 
+;;; ==========================
+;;; Garbage Collection testing
+;;; default value is 800000 (i.e., ~800kB).
+;;; ==========================
+
+;;; --- ideasman ---
 ;; ideasman_42's suggestion
 ;; https://www.reddit.com/r/emacs/comments/msll0j/do_any_of_you_have_some_tips_on_speeding_up_emacs/
 ;; default of gc-cons-threshold is 800k
 ;(defvar default-gc-cons-threshold 16777216 ; 16mb
 ;  "my default desired value of `gc-cons-threshold' during normal emacs operations.")
 
-(setq gc-cons-threshold (* 100 1000 10000))
+;;; ---
+;(setq gc-cons-threshold (* 100 1000 10000))
+
+;;; ---
+;; make garbage collector less invasive
+;(setq
+;  gc-cons-threshold most-positive-fixnum
+;  gc-cons-percentage 0.6)
+
+;;; ---
+;;; https://emacs.stackexchange.com/questions/34342/is-there-any-downside-to-setting-gc-cons-threshold-very-high-and-collecting-ga
+;; Set garbage collection threshold
+(setq gc-cons-threshold-original gc-cons-threshold)
+(setq gc-cons-threshold (* 1024 1024 100))
+;; Set file-name-handler-alist
+(setq file-name-handler-alist-original file-name-handler-alist)
+(setq file-name-handler-alist nil)
+;; Reset back to original values when idle
+(run-with-idle-timer
+ 5 nil
+ (lambda ()
+   (setq gc-cons-threshold gc-cons-threshold-original)
+   (setq file-name-handler-alist file-name-handler-alist-original)
+   (makunbound 'gc-cons-threshold-original)
+   (makunbound 'file-name-handler-alist-original)
+   (message "gc-cons-threshold and file-name-handler-alist restored")))
+
+;;; ==========================
+
 (add-hook 'emacs-startup-hook
           #'(lambda ()
               (message "Startup in %s sec with %d garbage collections"
                        (emacs-init-time "%.2f")
                        gcs-done)))
 
-;; make garbage collector less invasive
-(setq
-  gc-cons-threshold
-  most-positive-fixnum
-  gc-cons-percentage 0.6)
 
 ;; Prevent the glimpse of un-styled Emacs by disabling these UI elements early.
 ;(menu-bar-mode -1)
 (tool-bar-mode -1)     ; comment to test emacs 29.1 mac hang
 ;(toggle-scroll-bar -1) ; comment to test emacs 29.1 mac hang
 
-;;; Temporarily disable the file name handler.
-(setq default-file-name-handler-alist file-name-handler-alist)
-(setq file-name-handler-alist nil)
+;;; Temporarily disable the file name handler. ;; Disabled as we're doing it above too
+;(setq default-file-name-handler-alist file-name-handler-alist)
+;(setq file-name-handler-alist nil)
 
 ;(add-hook 'emacs-startup-hook
 ;  (lambda (&rest _)
