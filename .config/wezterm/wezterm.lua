b317755d7d5adf56c5bae561e85a090d00a4bd5c
Douglas Jackson <djackson@grubhub.com>
2024-11-06 09:31:18 -0600

    Removed scheme-cycle function for now

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -40,42 +40,6 @@ config.hide_tab_bar_if_only_one_tab = true
 -- config.macos_window_background_blur = 10
 
 
----cycle through builtin dark schemes in dark mode,
----and through light schemes in light mode
--- https://github.com/wez/wezterm/discussions/3426
-local function themeCycler(window, _)
-	local allSchemes = wezterm.color.get_builtin_schemes()
-	local currentMode = wezterm.gui.get_appearance()
-	local currentScheme = window:effective_config().color_scheme
-	local darkSchemes = {}
-	local lightSchemes = {}
-
-	for name, scheme in pairs(allSchemes) do
-		if scheme.background then
-			local bg = wezterm.color.parse(scheme.background) -- parse into a color object
-			---@diagnostic disable-next-line: unused-local
-			local h, s, l, a = bg:hsla() -- and extract HSLA information
-			if l < 0.4 then
-				table.insert(darkSchemes, name)
-			else
-				table.insert(lightSchemes, name)
-			end
-		end
-	end
-	local schemesToSearch = currentMode:find("Dark") and darkSchemes or lightSchemes
-
-	for i = 1, #schemesToSearch, 1 do
-		if schemesToSearch[i] == currentScheme then
-			local overrides = window:get_config_overrides() or {}
-			overrides.color_scheme = schemesToSearch[i+1]
-			wezterm.log_info("Switched to: " .. schemesToSearch[i+1])
-			window:set_config_overrides(overrides)
-			return
-		end
-	end
-end
-
-
 -- Add more configuration here
 --
 config.use_fancy_tab_bar = false
@@ -95,12 +59,6 @@ end
 
 config.keys = {
   -- ... add these new entries to your config.keys table
-
-  -- Theme Cycler
-  { key = "t", mods = "ALT", action = wezterm.action_callback(themeCycler) },
-  -- Look up name of color scheme you switched to
-  { key = "Escape", mods = "CTRL", action = wezterm.action.ShowDebugOverlay },
-
   -- Redefine cwd for new windows, but not tabs
   { key = 'n', mods = 'CMD', action = wezterm.action.SpawnCommandInNewWindow { cwd=wezterm.home_dir } },
   {
